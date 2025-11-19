c682dfd16831f9fd3a93bf3e599cc67e3c485c76
reshmarevyrie <shremareshma@gmail.com>
2025-11-19 09:32:04 +0530

    javascript variable - var

---
+++ play.js
@@ -0,0 +1,8 @@
+// variables
+var fullName = "Reshma"; //string
+console.log(fullName);
+var age = 29; //number
+console.log(age);
+var hasHobbies = true; //boolean
+console.log(hasHobbies);
+// node play.js
