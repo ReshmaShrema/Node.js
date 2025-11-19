aa22b73171e6f94c506e41915f28999c1f82e180
reshmarevyrie <shremareshma@gmail.com>
2025-11-19 09:49:15 +0530

    defining and calling function

--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-// variables
+// Global variables
 var fullName = "Reshma"; //string
 console.log(fullName);
 var age = 29; //number
@@ -6,3 +6,18 @@ console.log(age);
 var hasHobbies = true; //boolean
 console.log(hasHobbies);
 // node play.js
+
+//function definition , pure function
+function summarizeUser(username, userAge, userHasHobbies) {
+  // local variable and argument  -> username, userAge, userHasHobbies
+  return (
+    "Name is " +
+    username +
+    " age is " +
+    userAge +
+    " and the user has hobbie " +
+    userHasHobbies
+  );
+}
+// function calling
+console.log(summarizeUser(fullName, age, hasHobbies));
